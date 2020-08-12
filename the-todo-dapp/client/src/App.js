e651fc87d153cf24396e6551ef41b99b44b4f9be
AM-77 <amine.griche77@protonmail.ch>
2020-08-12 16:51:32 +0200

    add client app

--- the-todo-dapp/client/src/App.js
+++ the-todo-dapp/client/src/App.js
@@ -1,73 +1,117 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
-
-import "./App.css";
+import React, { Component } from "react"
+import TodoContract from "./contracts/Todo.json"
+import getWeb3 from "./getWeb3"
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  
+  constructor(props) {
+    super(props)
+    
+    this.state = { 
+      storageValue: 0, 
+      web3: null, 
+      accounts: null, 
+      contract: null,
+      todo: "",
+      todos: []
+    }
+  }
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
+      const web3 = await getWeb3()
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
+      const accounts = await web3.eth.getAccounts()
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const networkId = await web3.eth.net.getId()
+      const deployedNetwork = TodoContract.networks[networkId]      
+      const instance = new web3.eth.Contract(TodoContract.abi, deployedNetwork && deployedNetwork.address )
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // Set web3, accounts and contract to the state.
+      this.setState({ web3, accounts, contract: instance }, this.initTodos)
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`)
+      console.error(error)
     }
-  };
+  }
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  initTodos = async () => {
+    const  { contract } = this.state
+    // Get the todo list from the smart contract and set it to the state.
+    const todos = await contract.methods.getTodos().call()
+    console.log("todos: ", todos)
+    if(todos) this.setState({ todos })
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  onInputChange = e => {
+    const value = e.currentTarget.value
+    const name = e.currentTarget.name
+    
+    this.setState({[name]: value})
+  }
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+  onFormSubmit = e => {
+    e.preventDefault()
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+    const { accounts, contract, todo } = this.state
+    contract.methods.addTodo(todo).send({ from: accounts[0] }).then(() => {
+      contract.methods.getTodos().call().then(todos => this.setState({ todos }))
+    })
+  }
+
+  onTodoCheck = id => {
+    const { accounts, contract } = this.state    
+    contract.methods.toggleTodo(id).send({ from: accounts[0] }).then(() => {
+      contract.methods.getTodos().call().then(todos => this.setState({ todos }))
+    })
+  }
+
+  onTodoDelete = id => {
+    console.log(id)
+    const { accounts, contract } = this.state    
+    contract.methods.deleteTodo(id).send({ from: accounts[0] }).then(() => {
+      contract.methods.getTodos().call().then(todos => this.setState({ todos }))
+    })
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
+    const { web3, todo, todos } = this.state
+        return <div className="app-container">
+        <div className="header">
+          <h1>The TODO DAPP</h1>
+        </div>
+        <div className="main">
+          {
+            web3 ?
+              <>
+                <div className="form">
+                  <form onSubmit={this.onFormSubmit}>
+                    <input type="text" placeholder="your todo" name="todo" value={todo} onChange={this.onInputChange} />
+                    <input value="+" type="submit" />
+                  </form>
+                </div>
+                <div className="todos">
+                  {
+                    todos.map((todo, index) => {
+                      if (todo.content !== "" )
+                        return <div key={index} className="todo">
+                            <input type="checkbox" onChange={() => this.onTodoCheck(todo.id)} checked={todo.done}/>
+                            <p className="content">{todo.content}</p>
+                            <button onClick={() => this.onTodoDelete(todo.id)} alt="delete">x</button>
+                          </div>
+                    })
+                  }
+                </div>
+              </>
+              :
+              <div className="error">
+                <p className="message">Failed to load web3, accounts, or contract. Check console for details.</p>
+              </div>
+          }
+        </div>
+    </div>
   }
 }
 
-export default App;
+export default App
